fadc81b770b943eb282e4d41758eefe9493e0724
Jason Chung <jvn4develop@gmail.com>
2021-03-08 22:30:46 +0900

    initial Flo-Project

---
+++ Flo/Controllers/LyricsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LyricsViewController.swift
+//  Flo
+//
+//  Created by 정성준 on 2021/03/08.
+//
+
+import UIKit
+
+class LyricsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ Flo/Controllers/PlayViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flo
+//
+//  Created by 정성준 on 2021/03/08.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
